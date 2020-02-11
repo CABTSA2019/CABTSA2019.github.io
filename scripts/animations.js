1e4d073389de09d79af2c36bb9ab8087ebd2a073
s.samuel.bernal <s.samuel.bernal@cab-107cte-17.redclay.k12.de.us>
2020-02-11 15:54:35 -0500

    almost done scroll locking

--- scripts/animations.js
+++ scripts/animations.js
@@ -20,21 +20,61 @@ $(document).ready(function(){
         }
     });
 
-    $("#clickham").click(function() {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#scroll3").offset().top
-        }, 1000);
+    var down = true;
+    var d = 0;
+    var u = 0;
+    $(window).bind('mousewheel', function(event) {
+        if (event.originalEvent.wheelDelta >= 0) {
+            console.log('Scroll up');
+            down = false;
+            d++;
+        }
+        else {
+            console.log('Scroll down');
+            down = true;
+            u++;
+        }
     });
 
+    var pos = 0;
     var x = 0;
     $( window ).scroll(function(){
         x += 1;
 
+        if (x > 3) {
+            if (pos == $("#scroll1").position().top && down) {
+                pos = $("#scroll2").position().top;
+            } else if (pos == $("#scroll2").position().top && down) {
+                pos = $("#scroll3").position().top;
+            } else if (pos == $("#scroll3").position().top && down) {
+                pos = $("#scroll4").position().top;
+            } else if (pos == $("#scroll4").position().top && down) {
+                pos = $("#scroll5").position().top;
+            } else if (pos == $("#scroll5").position().top && down) {
+                pos = $("#scroll6").position().top;
+            } else if (pos == $("#scroll6").position().top && !down) {
+                pos = $("#scroll5").position().top;
+            } else if (pos == $("#scroll5").position().top && !down) {
+                pos = $("#scroll4").position().top;
+            } else if (pos == $("#scroll4").position().top && !down) {
+                pos = $("#scroll3").position().top;
+            } else if (pos == $("#scroll3").position().top && !down) {
+                pos = $("#scroll2").position().top;
+            } else if (pos == $("#scroll2").position().top && !down) {
+                pos = $("#scroll1").position().top;
+            }
+            x = 0;
+        }
+
+        $("#testy").text(pos);
+        $("#testy2").text(x);
+        $("#testy3").text(down);
+        $("#testy4").text(d);
         $body.style.removeProperty('overflow');
-    $body.style.removeProperty('position');
-    $body.style.removeProperty('top');
-    $body.style.removeProperty('width');
-    window.scrollTo(0, scrollPosition);
+        $body.style.removeProperty('position');
+        $body.style.removeProperty('top');
+        $body.style.removeProperty('width');
+        window.scrollTo(2000, pos);
     });
 
 });
