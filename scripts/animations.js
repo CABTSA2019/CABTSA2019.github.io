fb0acbecd0ace6d4bf2afae27e4f61c4c10e3229
s.samuel.bernal <s.samuel.bernal@cab-107cte-17.redclay.k12.de.us>
2020-02-12 11:43:57 -0500

    idk i hate javascript

--- scripts/animations.js
+++ scripts/animations.js
@@ -21,55 +21,76 @@ $(document).ready(function(){
     });
 
     var down = true;
-    var d = 0;
-    var u = 0;
+
     $(window).bind('mousewheel', function(event) {
         if (event.originalEvent.wheelDelta >= 0) {
-            console.log('Scroll up');
             down = false;
-            d++;
         }
         else {
-            console.log('Scroll down');
             down = true;
-            u++;
         }
     });
 
     var pos = 0;
     var x = 0;
-    $( window ).scroll(function(){
-        x += 1;
+    var scrollOnce = false;
 
-        if (x > 3) {
-            if (pos == $("#scroll1").position().top && down) {
+    function endScroll() {
+        scrollOnce = true;
+        console.log("true");
+    }
+
+    $( window ).scroll(function(){
+        x++;
+
+        console.log(x);
+
+        if (x == 10 && !scrollOnce) {
+            console.log("stupid");
+            if (pos == $("#scroll1").position().top && down && !scrollOnce) {
                 pos = $("#scroll2").position().top;
-            } else if (pos == $("#scroll2").position().top && down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll2").position().top && down && !scrollOnce) {
                 pos = $("#scroll3").position().top;
-            } else if (pos == $("#scroll3").position().top && down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll3").position().top && down && !scrollOnce) {
                 pos = $("#scroll4").position().top;
-            } else if (pos == $("#scroll4").position().top && down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll4").position().top && down && !scrollOnce) {
                 pos = $("#scroll5").position().top;
-            } else if (pos == $("#scroll5").position().top && down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll5").position().top && down && !scrollOnce) {
                 pos = $("#scroll6").position().top;
-            } else if (pos == $("#scroll6").position().top && !down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll6").position().top && !down && !scrollOnce) {
                 pos = $("#scroll5").position().top;
-            } else if (pos == $("#scroll5").position().top && !down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll5").position().top && !down && !scrollOnce) {
                 pos = $("#scroll4").position().top;
-            } else if (pos == $("#scroll4").position().top && !down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll4").position().top && !down && !scrollOnce) {
                 pos = $("#scroll3").position().top;
-            } else if (pos == $("#scroll3").position().top && !down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll3").position().top && !down && !scrollOnce) {
                 pos = $("#scroll2").position().top;
-            } else if (pos == $("#scroll2").position().top && !down) {
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
+            } else if (pos == $("#scroll2").position().top && !down && !scrollOnce) {
                 pos = $("#scroll1").position().top;
+                $("html, body").animate({ scrollTop: pos }, 500);
+                console.log("idk");
             }
+            endScroll();
             x = 0;
         }
-
-        $("#testy").text(pos);
-        $("#testy2").text(x);
-        $("#testy3").text(down);
-        $("#testy4").text(d);
         $body.style.removeProperty('overflow');
         $body.style.removeProperty('position');
         $body.style.removeProperty('top');
