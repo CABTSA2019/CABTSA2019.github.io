7768e56b94b23182d57fd6fede50a01fe65101de
sembernak <samcori02@gmail.com>
2020-02-16 00:07:15 -0500

    scrolling and menu animations complete

--- scripts/animations.js
+++ scripts/animations.js
@@ -31,71 +31,136 @@ $(document).ready(function(){
         }
     });
 
+    function getScrollbarWidth() {
+
+      // Creating invisible container
+      const outer = document.createElement('div');
+      outer.style.visibility = 'hidden';
+      outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+      outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+      document.body.appendChild(outer);
+
+      // Creating inner element and placing it in the container
+      const inner = document.createElement('div');
+      outer.appendChild(inner);
+
+      // Calculating difference between container's full width and the child width
+      const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+      // Removing temporary elements from the DOM
+      outer.parentNode.removeChild(outer);
+
+      return scrollbarWidth;
+
+    }
+
+    var swidth = getScrollbarWidth();
+
     var pos = 0;
     var x = 0;
     var scrollOnce = false;
 
-    function endScroll() {
-        scrollOnce = true;
-        console.log("true");
+    function oneScroll() {
+        return false;
     }
 
     $( window ).scroll(function(){
         x++;
 
-        console.log(x);
-
-        if (x == 10 && !scrollOnce) {
-            console.log("stupid");
-            if (pos == $("#scroll1").position().top && down && !scrollOnce) {
-                pos = $("#scroll2").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll2").position().top && down && !scrollOnce) {
-                pos = $("#scroll3").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll3").position().top && down && !scrollOnce) {
-                pos = $("#scroll4").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll4").position().top && down && !scrollOnce) {
-                pos = $("#scroll5").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll5").position().top && down && !scrollOnce) {
-                pos = $("#scroll6").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll6").position().top && !down && !scrollOnce) {
-                pos = $("#scroll5").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll5").position().top && !down && !scrollOnce) {
-                pos = $("#scroll4").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll4").position().top && !down && !scrollOnce) {
-                pos = $("#scroll3").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll3").position().top && !down && !scrollOnce) {
-                pos = $("#scroll2").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            } else if (pos == $("#scroll2").position().top && !down && !scrollOnce) {
-                pos = $("#scroll1").position().top;
-                $("html, body").animate({ scrollTop: pos }, 500);
-                console.log("idk");
-            }
-            endScroll();
+        if (x > 20) {
             x = 0;
         }
-        $body.style.removeProperty('overflow');
+
+        if (x == 20 && !scrollOnce) {
+            console.log("stupid");
+            if ( (pos < $("#scroll2").position().top && pos >= 0) && down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll2").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll2").position().top && pos < $("#scroll3").position().top && down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll3").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll3").position().top && pos < $("#scroll4").position().top && down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll4").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll4").position().top && pos < $("#scroll5").position().top && down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll5").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll5").position().top && pos < $("#scroll6").position().top && down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll6").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll6").position().top && !down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll5").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll5").position().top && pos < $("#scroll6").position().top && !down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll4").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll4").position().top && pos < $("#scroll5").position().top && !down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll3").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll3").position().top && pos < $("#scroll4").position().top && !down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll2").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            } else if (pos >= $("#scroll2").position().top && pos < $("#scroll3").position().top && !down && !scrollOnce) {
+                scrollOnce = true;
+                pos = $("#scroll1").position().top;
+                $("html, body").animate({ scrollTop: pos }, 1500);
+                setTimeout(function(){
+                    scrollOnce = oneScroll();
+                    x = 0;
+                }, 2000);
+            }
+        }
+
         $body.style.removeProperty('position');
         $body.style.removeProperty('top');
         $body.style.removeProperty('width');
-        window.scrollTo(2000, pos);
+        window.scrollTo(0, pos);
     });
 
 });
+
