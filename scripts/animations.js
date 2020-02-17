807850aece0db85d298aa4cd5719e3e63839e92e
sembernak <samcori02@gmail.com>
2020-02-17 14:10:18 -0500

    final page working

--- scripts/animations.js
+++ scripts/animations.js
@@ -6,17 +6,22 @@ $(document).ready(function(){
     var open = false;
     $("#clickham").click(function(){
         if (!open) {
-            $("#ham-menu").animate({left: '0px'}, "fast");
             open = true;
+            $("#ham-menu").css("left", "calc(100vw + 300px)")
             $("#ham").attr("src", "images/acksw.png");
             $("#ham").css("width", "38px");
             $("#dimmer").css("visibility", "visible");
+            $("#nav-pos").css("left", "0vw");
+            $("#ham-menu").animate({left: '100vw'}, "fast");
+            $("#nav-cont").css("width", "300px");
         } else {
-            $("#ham-menu").animate({left: '300px'}, "fast");
             open = false;
             $("#ham").attr("src", "images/blackham.png");
             $("#ham").css("width", "50px");
             $("#dimmer").css("visibility", "hidden");
+            $("#nav-pos").css("left", "100vw");
+            $("#ham-menu").animate({left: '300px'}, "fast");
+            $("#nav-cont").css("width", "0px");
         }
     });
 
@@ -67,92 +72,91 @@ $(document).ready(function(){
     $( window ).scroll(function(){
         x++;
 
-        if (x > 20) {
+        if (x > 10) {
             x = 0;
         }
 
-        if (x == 20 && !scrollOnce) {
-            console.log("stupid");
+        if (x == 10 && !scrollOnce) {
             if ( (pos < $("#scroll2").position().top && pos >= 0) && down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll2").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll2").position().top && pos < $("#scroll3").position().top && down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll3").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll3").position().top && pos < $("#scroll4").position().top && down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll4").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll4").position().top && pos < $("#scroll5").position().top && down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll5").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll5").position().top && pos < $("#scroll6").position().top && down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll6").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll6").position().top && !down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll5").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll5").position().top && pos < $("#scroll6").position().top && !down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll4").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll4").position().top && pos < $("#scroll5").position().top && !down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll3").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll3").position().top && pos < $("#scroll4").position().top && !down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll2").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             } else if (pos >= $("#scroll2").position().top && pos < $("#scroll3").position().top && !down && !scrollOnce) {
                 scrollOnce = true;
                 pos = $("#scroll1").position().top;
-                $("html, body").animate({ scrollTop: pos }, 1500);
+                $("html, body").animate({ scrollTop: pos }, 1000);
                 setTimeout(function(){
                     scrollOnce = oneScroll();
                     x = 0;
-                }, 2000);
+                }, 1100);
             }
         }
 
@@ -164,3 +168,268 @@ $(document).ready(function(){
 
 });
 
+var sum = 0;
+var coloradd = 0;
+var extradd = 0;
+var trimadd = 42000;
+
+function resetOne() {
+    $("#col1").attr("src", "images/whiteuns.png");
+    $("#col2").attr("src", "images/blackuns.png");
+    $("#col3").attr("src", "images/blueuns.png");
+    $("#col4").attr("src", "images/orangeuns.png");
+    $("#col5").attr("src", "images/reduns.png");
+    $("#col6").attr("src", "images/grayuns.png");
+}
+
+function colorClick1() {
+    resetOne();
+    $("#col1").attr("src", "images/whitesel.png");
+    coloradd = 0;
+}
+
+function colorClick2() {
+    resetOne();
+    $("#col2").attr("src", "images/blacksel.png");
+    coloradd = 0;
+}
+
+function colorClick3() {
+    resetOne();
+    $("#col3").attr("src", "images/bluesel.png");
+    coloradd = 0;
+}
+
+function colorClick4() {
+    resetOne();
+    $("#col4").attr("src", "images/orangesel.png");
+    coloradd = 1000;
+}
+
+function colorClick5() {
+    resetOne();
+    $("#col5").attr("src", "images/redsel.png");
+    coloradd = 1000;
+}
+
+function colorClick6() {
+    resetOne();
+    $("#col6").attr("src", "images/graysel.png");
+    coloradd = 1500;
+}
+
+var exOne = false;
+var exTwo = false;
+var exThree = false;
+var exFour = false;
+var exFive = false;
+var exSix = false;
+
+function extraClick1() {
+    if (!exOne) {
+        $("#acc1").attr("src", "images/matstrue.png");
+        extradd += 120;
+        exOne = true;
+    } else {
+        $("#acc1").attr("src", "images/mats.png");
+        extradd -= 120;
+        exOne = false;
+    }
+}
+
+function extraClick2() {
+    if (!exTwo) {
+        $("#acc2").attr("src", "images/racktrue.png");
+        extradd += 320;
+        exTwo = true;
+    } else {
+        $("#acc2").attr("src", "images/rack.png");
+        extradd -= 320;
+        exTwo = false;
+    }
+}
+
+function extraClick3() {
+    if (!exThree) {
+        $("#acc3").attr("src", "images/cargotrue.png");
+        extradd += 65;
+        exThree = true;
+    } else {
+        $("#acc3").attr("src", "images/cargo.png");
+        extradd -= 65;
+        exThree = false;
+    }
+}
+
+function extraClick4() {
+    if (!exFour) {
+        $("#acc4").attr("src", "images/sunshieldtrue.png");
+        extradd += 55;
+        exFour = true;
+    } else {
+        $("#acc4").attr("src", "images/sunshield.png");
+        extradd -= 55;
+        exFour = false;
+    }
+}
+
+function extraClick5() {
+    if (!exFive) {
+        $("#acc5").attr("src", "images/kittrue.png");
+        extradd += 50;
+        exFive = true;
+    } else {
+        $("#acc5").attr("src", "images/kit.png");
+        extradd -= 50;
+        exFive = false;
+    }
+}
+
+function extraClick6() {
+    if (!exSix) {
+        $("#acc6").attr("src", "images/guardstrue.png");
+        extradd += 110;
+        exSix = true;
+    } else {
+        $("#acc6").attr("src", "images/guards.png");
+        extradd -= 110;
+        exSix = false;
+    }
+}
+
+function tOneClick() {
+    $("#t1").prop("checked", true);
+    $("#t2").prop("checked", false);
+    $("#t3").prop("checked", false);
+    trimadd = 42000;
+}
+
+function tTwoClick() {
+    $("#t1").prop("checked", false);
+    $("#t2").prop("checked", true);
+    $("#t3").prop("checked", false);
+    trimadd = 51000;
+}
+
+function tThreeClick() {
+    $("#t1").prop("checked", false);
+    $("#t2").prop("checked", false);
+    $("#t3").prop("checked", true);
+    trimadd = 55000;
+}
+
+var reg = true;
+var loan = false;
+var fin = false;
+
+function rClick() {
+    reg = true;
+    loan = false;
+    fin = false;
+    $(".ifloan").css("visibility", "hidden");
+    $(".ifloan").css("height", "0px");
+    $(".ifloan").css("width", "0px");
+    $(".iflease").css("visibility", "hidden");
+    $(".iflease").css("height", "0px");
+    $(".iflease").css("width", "0px");
+    $(".ifreg").css("visibility", "visible");
+    $(".ifreg").css("height", "auto");
+    $(".ifreg").css("width", "auto");
+    $("#direct").css("background-color", "black");
+    $("#direct").css("color", "white");
+    $("#finance").css("background-color", "white");
+    $("#finance").css("color", "black");
+    $("#lease").css("background-color", "white");
+    $("#lease").css("color", "black");
+    priceIn();
+}
+
+function lClick() {
+    reg = false;
+    loan = true;
+    fin = false;
+    $(".ifloan").css("visibility", "visible");
+    $(".ifloan").css("height", "auto");
+    $(".ifloan").css("width", "auto");
+    $(".iflease").css("visibility", "hidden");
+    $(".iflease").css("height", "0px");
+    $(".iflease").css("width", "0px");
+    $(".ifreg").css("visibility", "hidden");
+    $(".ifreg").css("height", "0px");
+    $(".ifreg").css("width", "0px");
+    $("#direct").css("background-color", "white");
+    $("#direct").css("color", "black");
+    $("#lease").css("background-color", "black");
+    $("#lease").css("color", "white");
+    $("#finance").css("background-color", "white");
+    $("#finance").css("color", "black");
+    priceIn();
+}
+
+function fClick() {
+    reg = false;
+    loan = false;
+    fin = true;
+    $(".ifloan").css("visibility", "hidden");
+    $(".ifloan").css("height", "0px");
+    $(".ifloan").css("width", "0px");
+    $(".iflease").css("visibility", "visible");
+    $(".iflease").css("height", "auto");
+    $(".iflease").css("width", "auto");
+    $(".ifreg").css("visibility", "hidden");;
+    $(".ifreg").css("height", "0px");
+    $(".ifreg").css("width", "0px");
+    $("#direct").css("background-color", "white");
+    $("#direct").css("color", "black");
+    $("#lease").css("background-color", "white");
+    $("#lease").css("color", "black");
+    $("#finance").css("background-color", "black");
+    $("#finance").css("color", "white");
+    priceIn();
+}
+
+function priceIn() {
+    var sum = coloradd + extradd + trimadd;
+    sum = sum - sum%1;
+    if (reg == true) {
+        var sumtemp = sum - 7500;
+        var toEnum = "";
+        if (sumtemp%1000 < 10) {
+            toEnum = "00";
+        } else if (sumtemp%1000 < 100) {
+            toEnum = "0";
+        }
+        var sendvalue = (sumtemp - sumtemp%1000)/1000 + "," + toEnum + sumtemp%1000;
+        $("#endvalue").text("$" + sendvalue);
+    } else if (loan == true) {
+        var sumtemp = (sum - 7500)/90;
+        sumtemp = sumtemp - sumtemp%1;
+        var toEnum = "";
+        if (sumtemp%1000 < 10) {
+            toEnum = "00";
+        } else if (sumtemp%1000 < 100) {
+            toEnum = "0";
+        }
+        var addon = "";
+        if (sumtemp >= 1000) {
+            addon = (sumtemp - sumtemp%1000)/1000 + ",";
+        }
+        var sendvalue = addon + toEnum + sumtemp%1000;
+        $("#endvalue").text("$" + sendvalue);
+    } else if (fin == true) {
+        var sumtemp = ((sum - 7500)*(Math.pow(1.04, 6)))/72;
+        sumtemp = sumtemp - sumtemp%1;
+        var toEnum = "";
+        if (sumtemp%1000 < 10) {
+            toEnum = "00";
+        } else if (sumtemp%1000 < 100) {
+            toEnum = "0";
+        }
+        var addon = "";
+        if (sumtemp >= 1000) {
+            addon = (sumtemp - sumtemp%1000)/1000 + ",";
+        }
+        var sendvalue = addon + toEnum + sumtemp%1000;
+        $("#endvalue").text("$" + sendvalue);
+    }
+}
