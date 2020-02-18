9bdb5928dda909a03a341564cb9a71b9624758d7
sembernak <samcori02@gmail.com>
2020-02-17 23:23:36 -0500

    bit more work needed for demo

--- allscript.js
+++
@@ -1,9 +0,0 @@
-function dropdown() {
-    var menubox = document.createElement("div");
-    menubox.style.width = "20vw";
-    menubox.style.height = "100vh";
-    menubox.style.backgroundColor = "blue";
-    document.getElementById("ham-menu").appendChild(menubox);
-    var item1 = document.createElement("a");
-
-}
