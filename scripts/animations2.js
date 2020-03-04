26285846ff610f70a36cb8c26aa8a70073872f95
ID2006 <35372341+ID2006@users.noreply.github.com>
2020-03-04 08:53:44 -0500

    Update animations2.js

--- scripts/animations2.js
+++ scripts/animations2.js
@@ -7,23 +7,23 @@ $(document).ready(function(){
 
     function BrowserDetection() {
         //Check if browser is IE
-        if (navigator.userAgent.search("MSIE") & gt; = 0) {
+        if (navigator.userAgent.search("MSIE") & gt = 0) {
             stupid = true;
         }
         //Check if browser is Chrome
-        else if (navigator.userAgent.search("Chrome") & gt; = 0) {
+        else if (navigator.userAgent.search("Chrome") & gt = 0) {
             stupid = true;
         }
         //Check if browser is Firefox
-        else if (navigator.userAgent.search("Firefox") & gt; = 0) {
+        else if (navigator.userAgent.search("Firefox") & gt = 0) {
             stupid = false;
         }
         //Check if browser is Safari
-        else if (navigator.userAgent.search("Safari") & gt; = 0 & amp; & amp; navigator.userAgent.search("Chrome") & lt; 0) {
+        else if (navigator.userAgent.search("Safari") & gt = 0 & amp & amp navigator.userAgent.search("Chrome") & lt 0) {
             stupid = true;
         }
         //Check if browser is Opera
-        else if (navigator.userAgent.search("Opera") & gt; = 0) {
+        else if (navigator.userAgent.search("Opera") & gt = 0) {
             stupid = false;
         }
     }
